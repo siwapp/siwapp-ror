a2c10472da2e1b751ef6815b0a3616d01af63b0b
peillis <enrique@markhaus.com>
2012-09-07 13:43:12 +0200

    showing items in invoice edit

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ActiveRecord::Base
   has_many :invoice_items, :dependent => :destroy
-  attr_accessible :base_amount, :customer_email, :customer_identification, :customer_name
+  attr_accessible :customer_name, :customer_identification, \
+    :customer_email, :invoicing_address, :shipping_address, \
+    :contact_person, :terms, :notes, :number, :issue_date, \
+    :due_date
+    
+  accepts_nested_attributes_for :invoice_items
 end
