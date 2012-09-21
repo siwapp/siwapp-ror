ae1fea8241d2f47a765331964b1c54ee07be8e4a
Joe Z <jzarate@gmail.com>
2012-09-21 17:19:41 -0400

    putting attr accesors back in. kudos for understanding

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,9 @@
 class Invoice < ActiveRecord::Base
   has_many :invoice_items, :dependent => :destroy
-
+  attr_accessible :customer_name, :customer_identification, \
+    :customer_email, :invoicing_address, :shipping_address, \
+    :contact_person, :terms, :notes, :number, :issue_date, \
+    :due_date, :invoice_items_attributes
+    
   accepts_nested_attributes_for :invoice_items
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,5 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
   has_and_belongs_to_many :taxes
+  attr_accessible :invoice_id, :description, :discount, :quantity, :unitary_cost
 end
