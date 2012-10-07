abf4cc16c92c1550ca29e687a6e0b7b0e3364b1e
peillis <enrique@markhaus.com>
2012-10-07 12:23:38 +0200

    validations for InvoiceItem

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,7 @@ class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
   has_and_belongs_to_many :taxes, class_name: 'Tax'
   attr_accessible :invoice_id, :description, :discount, :quantity, :unitary_cost, :tax_ids
+  
+  validates :invoice_id, :presence => true
+  validates :discount, :quantity, :unitary_cost, :numericality => true
 end
