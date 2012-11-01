b055f40e235591bdcb342a8bf34f3283be0dc857
peillis <enrique@markhaus.com>
2012-11-01 12:31:17 +0100

    more on invoice edition

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,6 +3,5 @@ class InvoiceItem < ActiveRecord::Base
   has_and_belongs_to_many :taxes, class_name: 'Tax'
   attr_accessible :invoice_id, :description, :discount, :quantity, :unitary_cost, :tax_ids
 
-  validates :invoice_id, :presence => true
   validates :discount, :quantity, :unitary_cost, :numericality => true
 end
