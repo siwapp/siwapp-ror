b19912e96d223ea8462b00160e745102670dfadd
Joe Z <jzarate@gmail.com>
2013-02-01 13:19:00 -0500

    #7 tax basic unit test

--- test/unit/tax_test.rb
+++ test/unit/tax_test.rb
@@ -1,6 +1,26 @@
 require 'test_helper'
 
 class TaxTest < ActiveSupport::TestCase
+  test "required fields" do
+    tax = Tax.new
+    assert !tax.save
+    tax = taxes(:one)
+    tax.name = nil
+    assert !tax.save
+  end
+
+  test "numerical fields" do
+    tax = taxes(:one) 
+    tax.value = 'xx'
+    assert !tax.save
+  end 
+
+  test "boolean fields" do
+    tax = taxes(:one) 
+    tax.is_default = nil
+    assert !tax.save
+  end
+
   # test "the truth" do
   #   assert true
   # end
