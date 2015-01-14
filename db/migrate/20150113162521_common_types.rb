d3c956463ba8d05fd92862c5f9877f8049691764
Enrique Martinez <enrique@markhaus.com>
2015-01-14 10:39:44 +0100

    migration

--- db/migrate/20150113162521_common_types.rb
+++ db/migrate/20150113162521_common_types.rb
@@ -24,8 +24,9 @@ class CommonTypes < ActiveRecord::Migration
     drop_table :sf_guard_user_group
     drop_table :sf_guard_user_permission
     drop_table :sf_guard_user_profile
+    drop_table :migration_version
 
-    change_column :common, :id, :integer
+    execute "ALTER TABLE common CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
     change_column :common, :recurring_invoice_id, :integer
     change_column :common, :series_id, :integer
     change_column :common, :customer_id, :integer
@@ -33,5 +34,38 @@ class CommonTypes < ActiveRecord::Migration
     change_column :common, :shipping_address, :text
     change_column :common, :terms, :text
     change_column :common, :notes, :text
+
+    execute "ALTER TABLE customer CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :customer, :invoicing_address, :text
+    change_column :customer, :shipping_address, :text
+
+    execute "ALTER TABLE item CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :item, :common_id, :integer
+    change_column :item, :product_id, :integer
+
+    change_column :item_tax, :item_id, :integer
+    change_column :item_tax, :tax_id, :integer
+
+    execute "ALTER TABLE payment CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :payment, :invoice_id, :integer
+    change_column :payment, :notes, :text
+
+    execute "ALTER TABLE product CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :product, :description, :text
+
+    change_column :property, :value, :text
+
+    execute "ALTER TABLE series CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+
+    execute "ALTER TABLE tag CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+
+    execute "ALTER TABLE tagging CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :tagging, :tag_id, :integer
+    change_column :tagging, :taggable_id, :integer
+
+    execute "ALTER TABLE tax CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+
+    execute "ALTER TABLE template CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;"
+    change_column :template, :template, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,25 +22,25 @@ ActiveRecord::Schema.define(version: 20150113162521) do
     t.text     "invoicing_address",       limit: 65535
     t.text     "shipping_address",        limit: 65535
     t.string   "contact_person",          limit: 100
-    t.text     "terms",                   limit: 4294967295
-    t.text     "notes",                   limit: 4294967295
-    t.decimal  "base_amount",                                precision: 53, scale: 15
-    t.decimal  "discount_amount",                            precision: 53, scale: 15
-    t.decimal  "net_amount",                                 precision: 53, scale: 15
-    t.decimal  "gross_amount",                               precision: 53, scale: 15
-    t.decimal  "paid_amount",                                precision: 53, scale: 15
-    t.decimal  "tax_amount",                                 precision: 53, scale: 15
+    t.text     "terms",                   limit: 65535
+    t.text     "notes",                   limit: 65535
+    t.decimal  "base_amount",                           precision: 53, scale: 15
+    t.decimal  "discount_amount",                       precision: 53, scale: 15
+    t.decimal  "net_amount",                            precision: 53, scale: 15
+    t.decimal  "gross_amount",                          precision: 53, scale: 15
+    t.decimal  "paid_amount",                           precision: 53, scale: 15
+    t.decimal  "tax_amount",                            precision: 53, scale: 15
     t.integer  "status",                  limit: 1
     t.string   "type",                    limit: 255
-    t.boolean  "draft",                   limit: 1,                                    default: true
-    t.boolean  "closed",                  limit: 1,                                    default: false
-    t.boolean  "sent_by_email",           limit: 1,                                    default: false
+    t.boolean  "draft",                   limit: 1,                               default: true
+    t.boolean  "closed",                  limit: 1,                               default: false
+    t.boolean  "sent_by_email",           limit: 1,                               default: false
     t.integer  "number",                  limit: 4
     t.integer  "recurring_invoice_id",    limit: 4
     t.date     "issue_date"
     t.date     "due_date"
     t.integer  "days_to_due",             limit: 3
-    t.boolean  "enabled",                 limit: 1,                                    default: false
+    t.boolean  "enabled",                 limit: 1,                               default: false
     t.integer  "max_occurrences",         limit: 4
     t.integer  "must_occurrences",        limit: 4
     t.integer  "period",                  limit: 4
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20150113162521) do
     t.date     "starting_date"
     t.date     "finishing_date"
     t.date     "last_execution_date"
-    t.datetime "created_at",                                                                           null: false
-    t.datetime "updated_at",                                                                           null: false
+    t.datetime "created_at",                                                                      null: false
+    t.datetime "updated_at",                                                                      null: false
   end
 
   add_index "common", ["contact_person"], name: "cntct_idx", using: :btree
@@ -67,8 +67,8 @@ ActiveRecord::Schema.define(version: 20150113162521) do
     t.string "identification",    limit: 50
     t.string "email",             limit: 100
     t.string "contact_person",    limit: 100
-    t.text   "invoicing_address", limit: 4294967295
-    t.text   "shipping_address",  limit: 4294967295
+    t.text   "invoicing_address", limit: 65535
+    t.text   "shipping_address",  limit: 65535
   end
 
   add_index "customer", ["name"], name: "cstm_idx", unique: true, using: :btree
@@ -77,10 +77,10 @@ ActiveRecord::Schema.define(version: 20150113162521) do
   create_table "item", force: :cascade do |t|
     t.decimal "quantity",                 precision: 53, scale: 15, default: 1.0, null: false
     t.decimal "discount",                 precision: 53, scale: 2,  default: 0.0, null: false
-    t.integer "common_id",    limit: 8
+    t.integer "common_id",    limit: 4
     t.string  "description",  limit: 255
     t.decimal "unitary_cost",             precision: 53, scale: 15, default: 0.0, null: false
-    t.integer "product_id",   limit: 8
+    t.integer "product_id",   limit: 4
   end
 
   add_index "item", ["common_id"], name: "common_id_idx", using: :btree
@@ -88,35 +88,29 @@ ActiveRecord::Schema.define(version: 20150113162521) do
   add_index "item", ["product_id"], name: "item_product_id_idx", using: :btree
 
   create_table "item_tax", id: false, force: :cascade do |t|
-    t.integer "item_id", limit: 8, default: 0, null: false
-    t.integer "tax_id",  limit: 8, default: 0, null: false
-  end
-
-  add_index "item_tax", ["item_id"], name: "item_tax_item_id_item_id", using: :btree
-
-  create_table "migration_version", id: false, force: :cascade do |t|
-    t.integer "version", limit: 4
+    t.integer "item_id", limit: 4, default: 0, null: false
+    t.integer "tax_id",  limit: 4, default: 0, null: false
   end
 
   create_table "payment", force: :cascade do |t|
-    t.integer "invoice_id", limit: 8
+    t.integer "invoice_id", limit: 4
     t.date    "date"
-    t.decimal "amount",                        precision: 53, scale: 15
-    t.text    "notes",      limit: 4294967295
+    t.decimal "amount",                   precision: 53, scale: 15
+    t.text    "notes",      limit: 65535
   end
 
   add_index "payment", ["invoice_id"], name: "invoice_id_idx", using: :btree
 
   create_table "product", force: :cascade do |t|
-    t.string   "reference",   limit: 100,                                                null: false
-    t.text     "description", limit: 4294967295
-    t.decimal  "price",                          precision: 53, scale: 15, default: 0.0, null: false
-    t.datetime "created_at",                                                             null: false
-    t.datetime "updated_at",                                                             null: false
+    t.string   "reference",   limit: 100,                                           null: false
+    t.text     "description", limit: 65535
+    t.decimal  "price",                     precision: 53, scale: 15, default: 0.0, null: false
+    t.datetime "created_at",                                                        null: false
+    t.datetime "updated_at",                                                        null: false
   end
 
   create_table "property", primary_key: "keey", force: :cascade do |t|
-    t.text "value", limit: 4294967295
+    t.text "value", limit: 65535
   end
 
   create_table "series", force: :cascade do |t|
@@ -140,9 +134,9 @@ ActiveRecord::Schema.define(version: 20150113162521) do
   add_index "tag", ["triple_value"], name: "triple3_idx", using: :btree
 
   create_table "tagging", force: :cascade do |t|
-    t.integer "tag_id",         limit: 8,  null: false
+    t.integer "tag_id",         limit: 4,  null: false
     t.string  "taggable_model", limit: 30
-    t.integer "taggable_id",    limit: 8
+    t.integer "taggable_id",    limit: 4
   end
 
   add_index "tagging", ["tag_id"], name: "tag_idx", using: :btree
@@ -157,9 +151,9 @@ ActiveRecord::Schema.define(version: 20150113162521) do
 
   create_table "template", force: :cascade do |t|
     t.string   "name",       limit: 255
-    t.text     "template",   limit: 4294967295
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.text     "template",   limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "slug",       limit: 255
     t.string   "models",     limit: 200
   end
