9ae752f89fef075cb91d87d369d8675d86414008
peillis <enrique@markhaus.com>
2015-02-09 21:54:06 +0100

    :customer_email not mandatory, and playing with FactoryGirl

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,8 @@
 class Invoice < Common
   belongs_to :recurring_invoice
   validates :customer_name, presence: true
-  validates :customer_email, presence: true, \
-  format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Only valid emails"}
+  validates :customer_email,
+    format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Only valid emails"}
   validates :number, presence: true, numericality: {only_integer: true}
 
   def to_s
--- spec/factories/invoice_factory.rb
+++ spec/factories/invoice_factory.rb
@@ -3,5 +3,14 @@ FactoryGirl.define do
     customer_name "Example Customer Name"
     customer_email 'example@example.com'
     number "1"
+
+    factory :invoice_random do
+      customer_name { ["Acme, inc.",
+                       "Widget Corp",
+                       "Warehousing",
+                       "Demo Company",
+                       "Smith and Co."
+      ].sample }
+    end
   end
 end
