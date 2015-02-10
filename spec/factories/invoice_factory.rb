25b94a470245a5fd2fa10f08487a07a85f52c86a
peillis <enrique@markhaus.com>
2015-02-10 13:39:04 +0100

    generating random data with FactoryGirl

--- spec/factories/invoice_factory.rb
+++ spec/factories/invoice_factory.rb
@@ -1,16 +1,24 @@
 FactoryGirl.define do
-  factory :invoice do
+  factory :common do
     customer_name "Example Customer Name"
     customer_email 'example@example.com'
     number "1"
 
-    factory :invoice_random do
-      customer_name { ["Acme, inc.",
-                       "Widget Corp",
-                       "Warehousing",
-                       "Demo Company",
-                       "Smith and Co."
-      ].sample }
+    factory :invoice do
+      type "Invoice"
+
+      factory :invoice_random do
+        customer_name { ["Acme, inc.",
+                         "Widget Corp",
+                         "Warehousing",
+                         "Demo Company",
+                         "Smith and Co."
+        ].sample }
+        after(:create) do |invoice|
+          create_list(:item, rand(10), common: invoice)
+        end
+      end
     end
+
   end
 end
--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     description 'Item example description'
     quantity 5
     unitary_cost 3.33
+    common
   end
 end
