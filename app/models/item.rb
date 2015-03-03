4bd0298ea41909c0883b088ff9e84c58914acfbe
Carlos Escribano <carlosescri@gmail.com>
2015-03-03 11:53:05 +0100

    Invoice updated_at changes when a child object is updated.
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  belongs_to :common
+  belongs_to :common, touch: true
   has_and_belongs_to_many :taxes
   accepts_nested_attributes_for :taxes
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ActiveRecord::Base
-  belongs_to :invoice
+  belongs_to :invoice, touch: true
 
   validates :invoice_id, presence: true, numericality: {only_integer: true}
 end
