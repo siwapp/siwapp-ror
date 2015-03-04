de8b46e4a48b9c7a9c978d14ae61d47b25503be9
Carlos Escribano <carlosescri@gmail.com>
2015-03-04 13:17:51 +0100

    Presence of series is mandatory for invoices
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,12 +7,9 @@ class Invoice < Common
     format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
              message: "Only valid emails"}, allow_blank: true
   validates :number, presence: true, numericality: {only_integer: true}
+  validates :serie, presence: true
 
   def to_s
-    if serie
-      "#{serie.value}-#{number}"
-    else
-      "XXX-#{number}"
-    end
+    "#{serie.value}-#{number}"
   end
 end
