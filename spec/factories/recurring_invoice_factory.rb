ac96fabd45e16049b8bb3283355a8d21280c9401
Carlos Escribano <carlosescri@gmail.com>
2015-03-04 17:35:11 +0100

    ws
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/factories/recurring_invoice_factory.rb
+++ spec/factories/recurring_invoice_factory.rb
@@ -9,12 +9,15 @@ FactoryGirl.define do
 
     factory :recurring_invoice_random do
       serie { Serie.all.sample || generate(:serie_random) }
+
       customer_name do
         ["Acme, inc.", "Widget Corp", "Warehousing", "Demo Company",
          "Smith and Co."].sample
       end
+
       starting_date { Date.today >> rand(-8..1) }
       finishing_date { Date.today >> rand(1..12) }
+
       after(:create) do |invoice|
         create_list(:item_random, rand(1..10), common: invoice)
       end
