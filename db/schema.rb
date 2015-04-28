2deb6d8e60d5acf44314c9941cd24de68730eb8d
Carlos Escribano <carlosescri@gmail.com>
2015-04-28 17:03:28 +0200

    Schema updated.
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323090229) do
+ActiveRecord::Schema.define(version: 20150428083035) do
 
   create_table "commons", force: :cascade do |t|
     t.integer  "serie_id",                limit: 4
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 20150323090229) do
     t.decimal "discount",                 precision: 53, scale: 2,  default: 0.0, null: false
     t.integer "common_id",    limit: 4,                                           null: false
     t.string  "description",  limit: 255
-    t.decimal "unitary_cost",             precision: 53, scale: 15, default: 0.0
+    t.decimal "unitary_cost",             precision: 53, scale: 15, default: 0.0, null: false
     t.integer "product_id",   limit: 4
   end
 
@@ -143,6 +143,26 @@ ActiveRecord::Schema.define(version: 20150323090229) do
 
   add_index "tagging", ["tag_id"], name: "tag_idx", using: :btree
   add_index "tagging", ["taggable_model", "taggable_id"], name: "taggable_idx", using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "taxes", force: :cascade do |t|
     t.string  "name",       limit: 50
