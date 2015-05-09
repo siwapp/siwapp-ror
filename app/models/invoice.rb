cf02f3e48fd77af842c14f53ae28d04db39abf46
serguitus <serguitus@gmail.com>
2015-05-09 01:08:17 -0400

    advanced search on partial

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -20,7 +20,7 @@ class Invoice < Common
 
   filterrific(#default_filter_params: { sorted_by: 'created_at_desc' },
               available_filters: [
-                                  #:sorted_by,
+                                  :with_serie_id,
                                   :search_query,
                                  ]
               )
