73ebd0dba9d5d60bfb0e24ef1a57f01211a44e35
peillis <enrique@markhaus.com>
2015-06-15 12:44:08 +0200

    editing templates

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -69,6 +69,6 @@ class TemplatesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def template_params
-      params[:template]
+      params.require(:template).permit(:name, :template)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   resources :customers
 
+  resources :templates
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
