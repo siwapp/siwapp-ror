996ea541a522b7cf2a5e2c3c8953bd0aede004ac
peillis <enrique@markhaus.com>
2015-06-18 14:26:36 +0200

    specs for pdf

---
+++ spec/factories/template_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :template do
+    name "default"
+    template "fake template"
+  end
+end
---
+++ spec/features/viewing_pdf_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'PDF of Invoices' do
+  before do
+    FactoryGirl.create(:invoice)
+    FactoryGirl.create(:template)
+    visit "/invoices"
+    click_link "ES-1"
+  end
+
+  scenario 'Show of a paid invoice show the iframe' do
+    expect(page).to have_xpath("//iframe")
+    expect(page).to have_link("PDF")
+    click_link "PDF"
+    expect(page.response_headers['Content-Type']).to eql "application/pdf"
+  end
+  
+  scenario 'Template url shows template' do
+    visit "/invoices/template/2/invoice/1"
+    expect(page).to have_content("fake template")
+  end
+    
+end
+
