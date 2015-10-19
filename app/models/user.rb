37a0c448b0f6fd6ec27104e50fb5f5ed27bf6eb5
serguitus <serguitus@gmail.com>
2015-10-18 20:51:27 -0400

    adding some validation to user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
     message: "Only valid emails"}, uniqueness: {case_sensitive: false}
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe User do
   describe 'valid user' do
     it "user can be created" do
-      user = User.new(name:"john Doe", email: "john@doe.com", password: 'pass',
-                      password_confirmation: 'pass' )
+      user = User.new(name:"john Doe", email: "john@doe.com", password: 'passkey',
+                      password_confirmation: 'passkey' )
       expect(user).to be_valid
     end
   end
