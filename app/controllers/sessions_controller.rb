de38e16e9903d732dbbcbe8551a73f69c626ccbd
ecoslado <kike@doofinder.com>
2015-10-20 12:23:24 +0200

    New button conditional.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to invoices_url
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to login_url
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,17 @@
+module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
