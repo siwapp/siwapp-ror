1e6eba16b703b79fdcd20ef561c3342abc6970ed
Enrique Martinez <enrique@markhaus.com>
2015-10-29 13:22:12 +0100

    some refactoring, and starting with settings edition

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
 
   resources :templates
 
+  get 'settings/global_configuration'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
