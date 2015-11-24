2d6c94d907fba71c50392dd7d9a6b3f8a69e8724
ecoslado <kike@doofinder.com>
2015-11-24 10:18:44 +0100

    Removes signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'signup' => 'users#new'
-
   get    'login'   => 'sessions#new',      as: :login
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy',  as: :logout
