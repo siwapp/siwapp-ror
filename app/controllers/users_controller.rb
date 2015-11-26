dc50c5e41f1b8adfe79815962b09bb1c928f2a2d
ecoslado <kike@doofinder.com>
2015-11-26 10:43:57 +0100

    Change password.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
+  before_filter :logged_in?
 
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+  	@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:info] = 'User updated correctly'	
+      redirect_to invoices_url
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
