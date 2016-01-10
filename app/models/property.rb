06720dcedbced5872facf458b36a924a0ba64f42
peillis <enrique@markhaus.com>
2016-01-10 18:48:06 +0100

    improving pdf

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,13 @@ class Property < ActiveRecord::Base
   def to_s
     "#{key}: #{value}"
   end
+
+  # Returns an object with all the settings stored in database
+  def self.all_settings
+    settings = {}
+    Property.all.each do |property|
+      settings[property.key] = property.value
+    end
+    settings
+  end
 end
