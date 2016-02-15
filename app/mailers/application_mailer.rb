01e727fb814faa17429ba5420352865f183ad7b2
peillis <enrique@markhaus.com>
2016-02-15 17:45:13 +0100

    smtp settings for mail delivery

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
+  self.delivery_method = :smtp
+  self.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               '',
+    user_name:            '',
+    password:             '',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+  self.raise_delivery_errors = true
   email_property = Property.find_by key: 'company_email'
   if email_property
     default from: email_property.value
--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,6 +1,9 @@
 class InvoiceMailer < ApplicationMailer
   def email_invoice(invoice)
     @invoice = invoice
-    mail(to: @invoice.email, subject: "Your invoice #{invoice}")
+    mail(
+      to: @invoice.email,
+      subject: "Your invoice #{invoice}"
+    )
   end
 end
