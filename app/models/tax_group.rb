4a08507ead0bb12a20fb39bf453fc4a0aad00f68
ecoslado <kike@doofinder.com>
2016-03-15 13:15:29 +0100

    Less is more.

--- app/models/tax_group.rb
+++
@@ -1,8 +0,0 @@
-class TaxGroup < ActiveRecord::Base
-	has_and_belongs_to_many :taxes
-	validates :name, presence: true
-
-	def to_s
-    	name
- 	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy',  as: :logout
 
-  resources :taxes, :series, :payments, :templates, :tax_groups
+  resources :taxes, :series, :payments, :templates
 
 
   get "invoices/amounts"
