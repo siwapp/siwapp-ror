5e61945cecd0b3c3a3990b65195d5d5a21993b11
ecoslado <kike@doofinder.com>
2016-03-18 11:30:03 +0100

    Seed for days_to_due

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ Settings.company_url = ""
 Settings.company_logo = ""
 Settings.currency = ""
 Settings.legal_terms = ""
+Settings.days_to_due = "0"
 
 Template.create(name: 'Default', template: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
