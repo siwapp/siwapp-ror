fa7111b389da368390d703f45e4ace3fb19bd97a
peillis <enrique@markhaus.com>
2016-04-02 18:05:44 +0200

    better tests for pdf

--- spec/features/viewing_pdf_spec.rb
+++ spec/features/viewing_pdf_spec.rb
@@ -9,12 +9,15 @@ feature 'PDF of Invoices' do
     click_link "ES-1"
   end
 
-  scenario 'Show of a paid invoice show the iframe' do
+  scenario 'Show of a paid invoice show the iframe', js: true, driver: :webkit do
     expect(page).to have_xpath("//iframe")
     expect(page).to have_link("PDF")
+    click_on "PDF"
+    expect(page.response_headers['Content-Disposition']).to eq 'attachment; filename="ES-1.pdf"'
+    expect(page.response_headers['Content-Type']).to eq 'application/pdf'
   end
 
-  scenario 'Template url shows template' do
+  scenario 'Template url shows template', js: true, driver: :webkit do
     visit "/invoices/template/1/invoice/1"
     expect(page).to have_content("BILLED ON")
   end
