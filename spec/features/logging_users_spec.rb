17d7ae92c1d6102f1878db29a7e1bd61cedf9250
Joe Z <jzarate@gmail.com>
2016-04-06 08:06:05 -0400

    users integration: logout link and login/out tests

--- spec/features/logging_users_spec.rb
+++ spec/features/logging_users_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
-feature 'Creating Invoices' do
+feature 'Logged users' do
 
   before do
     FactoryGirl.create :user
-  end
-
-  scenario 'can log in' do
     visit '/login'
     fill_in 'Email', with: 'testuser@example.org'
     fill_in 'Password', with: 'testuser'
     click_on 'Log in'
     expect(page.current_path).to eql invoices_path
   end
+
+  scenario 'can access anywhere' do
+    visit '/customers'
+    expect(page.current_path).to eql customers_path
+    visit '/recurring_invoices'
+    expect(page.current_path).to eql recurring_invoices_path
+    visit '/'
+    expect(page.current_path).to eql root_path
+  end
+
+  scenario 'and of course can log out' do
+    click_on 'Log out'
+    expect(page.current_path).to eql login_path
+    visit '/invoices'
+    expect(page.current_path).to eql login_path # where are you going, sailor?
+  end
+
 end
