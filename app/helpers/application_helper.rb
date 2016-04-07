b470b5e32abfde0c13e367ca75bbe1b069467245
Joe Z <jzarate@gmail.com>
2016-04-07 12:53:59 -0400

    refactor currency display code. fixes #80

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,7 @@ module ApplicationHelper
   end
 
   def display_money(amount)
-    currency_code = Settings.currency? ? Settings.currency : :eur
-    currency = Money::Currency.find currency_code
+    currency = Money::Currency.find(Settings.currency) || Money::Currency.find(:eur)
     format = currency.symbol_first? ? "%u %n" : "%n %u"
     negative_format = "(#{format})"
     number_to_currency amount, precision: currency.exponent.to_int, unit: currency.symbol,
