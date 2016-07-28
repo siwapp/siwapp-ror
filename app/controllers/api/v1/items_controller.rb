a56d15719199e7c4734ffc3f9457853836917681
Joe Z <jzarate@gmail.com>
2016-07-27 23:50:36 -0400

    #123 items update

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -16,17 +16,7 @@ class Api::V1::ItemsController < Api::V1::BaseController
     @item = Item.new(item_params)
     @item.update common_id: params[:invoice_id] || params[:recurring_invoice_id]
     # alternative taxes format
-    if params[:item].has_key? :taxes
-      params[:item][:taxes].each do |t|
-        begin
-          tax = Tax.find_by_name t
-          if !@item.taxes.exists? tax
-            @item.taxes<< tax
-          end
-        rescue ActiveRecord::RecordNotFound
-        end
-      end
-    end
+    add_human_taxes @item, params
     respond_to do |format|
       if @item.save
         format.json { redirect_to api_v1_item_url(@item) }
@@ -36,9 +26,36 @@ class Api::V1::ItemsController < Api::V1::BaseController
     end
   end
 
+  # PATH/PUTH /api/v1/items/:id
+  def update
+    respond_to do |format|
+      @item = Item.find params[:id]
+      if @item.update(item_params)
+#        add_human_taxes @item, params
+#        format.json { redirect_to api_v1_item_url(@item) }
+#        format.json { render action: show, status: ok, location: api_v1_item_url(@item) }
+        format.json { render action: 'show', status: :ok, location: api_v1_item_url(@item) }
+      else
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:description, :quantity, :unitary_cost, :discount, {:tax_ids => []})
+  end
+
+  # check if there's a 'taxes' array with taxes names and adds them to item
+  def add_human_taxes item, params
+    if params.has_key? :item and params[:item].has_key? :taxes
+      params[:item][:taxes].each do |tax_name|
+        tax = Tax.find_by_name tax_name
+        if tax and !item.taxes.exists? tax.id
+          item.taxes<< tax
+        end
+      end
+    end
   end
 
 end
