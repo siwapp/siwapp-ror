cc4d042b84505ad23d38459c0638a6d6f9175a5a
ecoslado <kike@doofinder.com>
2016-09-26 13:58:07 +0200

    Update invoice.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,13 +1,18 @@
 class Payment < ActiveRecord::Base
   acts_as_paranoid
-  belongs_to :invoice, touch: true  # "touch" changes invoice's updated_at on save
-
+  belongs_to :invoice
   validates :date, presence: true
+  
+  after_save do 
+  	invoice.save
+  end
 
   def to_jbuilder
     Jbuilder.new do |json|
       json.(self, :date, :amount, :notes)
     end
   end
+
+
 
 end
