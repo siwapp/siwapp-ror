bf5ca61846f8b166eac5bf46d16bc3ddb06a007c
ecoslado <kike@doofinder.com>
2016-09-30 12:27:21 +0200

    Fixing tests.

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -7,7 +7,7 @@ class InvoiceMailer < ApplicationMailer
   
   def email_invoice(invoice)
     @invoice = invoice
-    html = render_to_string :inline => Template.find_by(default: true).template,
+    html = render_to_string :inline => Template.find_by(email_default: true).template,
       :locals => {:invoice => @invoice, :settings => Settings}
     attachments["#{@invoice}.pdf"] = @invoice.pdf(html)
     mail(
