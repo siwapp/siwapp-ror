011572e9eecf715ba6fb7c267201cbfd55b4aeb7
ecoslado <kike@doofinder.com>
2016-10-26 11:17:33 +0200

    Selecting template from invoice creation. E-mail pdf well done.

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -9,22 +9,30 @@ class InvoiceMailer < ApplicationMailer
     @invoice = invoice
 
     if @invoice.email_template
-      template = @invoice.email_template.template
+      email_template = @invoice.email_template.template
     else
-      template = Template.find_by(email_default: true).template
+      email_template = Template.find_by(email_default: true).template
     end
-    html = render_to_string :inline => template,
 
+    if @invoice.print_template
+      print_template = @invoice.print_template.template
+    else
+      print_template = Template.find_by(print_default: true).template
+    end
+    
+    pdf_html = render_to_string :inline => print_template,
       :locals => {:invoice => @invoice, :settings => Settings}
-    attachments["#{@invoice}.pdf"] = @invoice.pdf(html)
+    email_body = render_to_string :inline => email_template,
+      :locals => {:invoice => @invoice, :settings => Settings}
+    attachments["#{@invoice}.pdf"] = @invoice.pdf(pdf_html)
     mail(
       from: Settings.company_email,
       # Just for testing
       to: "kike@doofinder.com", #@invoice.email,
       subject: Settings.email_subject,
-      body: html
+      body: email_body
     ) do |format|
-      format.html {html}
+      format.html {email_body}
     end
   end
 end
