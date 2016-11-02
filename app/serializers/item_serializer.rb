983c6c354170d8fb092f96ad73c4d5ee04fac3eb
ecoslado <kike@doofinder.com>
2016-11-02 11:25:55 +0100

    Adding tax_ids to item

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,10 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :description, :quantity, :unitary_cost, :discount
+  attributes :id, :description, :quantity, :unitary_cost, :discount, :tax_ids
   belongs_to :common
   has_many :taxes, links: {self: true, related: true}
+
+  def tax_ids
+     object.taxes.map {|t| t.id}
+  end
 
 end
