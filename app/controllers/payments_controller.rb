acc84dec263b5d9d958b5839449375ffe1b6f346
ecoslado <kike@doofinder.com>
2016-11-29 14:24:06 +0100

    Tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -33,8 +33,10 @@ class PaymentsController < ApplicationController
     respond_to do |format|
       if @payment.save
         format.html { redirect_to @payment, notice: 'Payment was successfully created.' }
+        format.json { render :show, status: :created, location: @payment }
       else
         format.html { render :new }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,8 +47,10 @@ class PaymentsController < ApplicationController
     respond_to do |format|
       if @payment.update(payment_params)
         format.html { redirect_to @payment, notice: 'Payment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @payment }
       else
         format.html { render :edit }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,6 +61,7 @@ class PaymentsController < ApplicationController
     @payment.destroy
     respond_to do |format|
       format.html { redirect_to payments_url, notice: 'Payment was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
