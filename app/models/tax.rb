31fec8ec4c2c24e95e13cbdac848e05394032484
Carlos Escribano <carlosescri@gmail.com>
2016-12-22 13:39:16 +0100

    #226 - Added missing test for taxes feature
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,16 +1,16 @@
 class Tax < ActiveRecord::Base
   acts_as_paranoid
   has_and_belongs_to_many :items, touch: true
-  before_destroy :has_items
+  before_destroy :check_is_not_used
 
   validates :name, presence: true
   validates :value, presence: true, numericality: true
 
   private
 
-  def has_items
+  def check_is_not_used
     if items.count > 0
-      errors.add(:base, "Can not delete a tax which is used in some invoices")
+      errors.add(:base, "Can't delete a tax which is used in some invoices")
       return false
     end
   end
