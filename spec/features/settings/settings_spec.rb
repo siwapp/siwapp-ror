fb44cd5af9548d3d76cf10e411373112e9ab77e0
Carlos Escribano <carlosescri@gmail.com>
2016-12-22 16:09:42 +0100

    #226 - Global Settings feature tests
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/features/settings/settings_spec.rb
+++ spec/features/settings/settings_spec.rb
@@ -6,36 +6,6 @@ feature 'Editing Settings' do
     visit "/settings/global"
   end
 
-  feature 'Global settings editing' do
-
-    before do
-      visit '/settings/global'
-    end
-
-    scenario 'Edit global settings', :js => true, driver: :webkit do
-      fill_in 'Company Name', with: 'NEW Company Name'
-      fill_in 'Company Email', with: 'bad@company.com'
-      select 'USD', from: 'global_settings_currency'
-      click_on 'Save'
-      expect(page).to have_content("Global settings successfully saved")
-      expect(Settings[:currency]).to eql 'usd'
-      expect(Settings[:company_email]).to eql 'bad@company.com'
-    end
-
-    scenario 'Can not update global settings badly', :js => true, driver: :webkit do
-      fill_in 'Days to due', with: ''
-      fill_in 'Company Email', with: 'bad@email.c'
-      click_on 'Save'
-      expect(page).to have_content("Company email bad format")
-      expect(page).to have_content("Days to due is not a number")
-      expect(page).to have_content(' Global settings could not be saved')
-      # nothing saved
-      expect(Settings[:days_to_due]).to eql 0 # the default setting
-      expect(Settings[:company_email]).to be nil # no saved
-    end
-
-  end
-
   feature 'SMTP settings editing' do
 
     before do
