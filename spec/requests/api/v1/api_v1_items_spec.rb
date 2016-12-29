4c701af002655fabe504226e16009e4cc6f06085
Carlos Escribano <carlosescri@gmail.com>
2016-12-29 18:42:56 +0100

    #226 - Fixing API tests
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/requests/api/v1/api_v1_items_spec.rb
+++ spec/requests/api/v1/api_v1_items_spec.rb
@@ -1,94 +1,90 @@
-require 'rails_helper'
+require "rails_helper"
 
-
-RSpec.describe "Api::V1::Items", type: :request do
-
+RSpec.describe "Api::V1::Items:", type: :request do
   before do
-    Rails.cache.clear
     FactoryGirl.create :token
     FactoryGirl.create :user
-    @headers = {'Content-Type' => 'application/json', 
-        'Authorization' => 'Token token="123token"'}
-    @customer = FactoryGirl.create(:customer, name: "Test Customer")
-    @series = FactoryGirl.create :series
-    @invoice = FactoryGirl.create :invoice, customer: @customer
+
+    @headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Token token=\"#{Settings.api_token}\""
+    }
+
+    @invoice = FactoryGirl.create(:invoice, :paid)
     @item = @invoice.items[0]
+    @taxes = Tax.all
   end
 
-  describe "Items show" do
-    it 'GET /api/v1/items/:id show single item with details' do
+  describe "Show:" do
+    it "GET /api/v1/items/:id -- Single item with details" do
       get api_v1_item_path(@item), nil, @headers
       expect(response).to be_success
       # invoice reference
-   #   expect(json['data']['relationships']['invoice']['links']['related']).to eql api_v1_invoice_url(@invoice)
+   #   expect(json["data"]["relationships"]["invoice"]["links"]["related"]).to eql api_v1_invoice_url(@invoice)
       # taxes
-   #   expect(json['data']['relationships']['taxes']['data'][0]['attributes']['name']).to eql 'VAT 21%'
-   #   expect(json['data']['relationships']['taxes']['data'][0]['attributes']['links']['related']).to eql api_v1_tax_url(@item.taxes[0])
+   #   expect(json["data"]["relationships"]["taxes"]["data"][0]["attributes"]["name"]).to eql "VAT 21%"
+   #   expect(json["data"]["relationships"]["taxes"]["data"][0]["attributes"]["links"]["related"]).to eql api_v1_tax_url(@item.taxes[0])
     end
   end
 
-  describe "Items listings" do
-    it 'GET /api/v1/invoices/:invoice_id/items provde listing' do
+  describe "List:" do
+    it "GET /api/v1/invoices/:invoice_id/items -- A list of items" do
       get api_v1_invoice_items_path(@invoice.id), nil, @headers
       expect(response).to be_success
-      expect(json["data"].length).to eql 3 # 3 items
-      expect(json["data"][0]["relationships"]['invoice']).to be_nil
-      expect(json["data"][0]["relationships"]['taxes']).to eql({"data" => [{"id"=>"1", "type"=>"taxes"}, {"id"=>"2", "type"=>"taxes"}]})
+      expect(json["data"].length).to eql 1 # 3 items
+      expect(json["data"][0]["relationships"]["invoice"]).to be_nil
+      expect(json["data"][0]["relationships"]["taxes"]).to eql({"data" => [{"id"=>"1", "type"=>"taxes"}, {"id"=>"2", "type"=>"taxes"}]})
     end
   end
 
   describe "Item creation" do
-
     before do
       @taxes = Tax.all
     end
 
-    it 'POST /api/v1/invoices/:invoice_id/items with taxes_id and taxes names' do
+    it "POST /api/v1/invoices/:invoice_id/items with taxes_id and taxes names" do
       item = {
-        'data' => {
-          'attributes' => {
-            'description' => 'brand new item',
-            'quantity' => 2,
-            'tax_ids' => [@taxes[0].id]
+        "data" => {
+          "attributes" => {
+            "description" => "brand new item",
+            "quantity" => 2,
+            "tax_ids" => [@taxes[0].id]
           }
         }
       }
       post api_v1_invoice_items_path(@invoice), item.to_json, @headers
       expect(response).to have_http_status :created
-      expect(json['data']['attributes']['tax-ids'][0]).to eql @taxes[0].id # tax assignment by id
-      # expect(json['data'][1]['attributes']['name']).to eql 'RETENTION' # tax assignment by name
+      expect(json["data"]["attributes"]["tax-ids"][0]).to eql @taxes[0].id # tax assignment by id
+      # expect(json["data"][1]["attributes"]["name"]).to eql "RETENTION" # tax assignment by name
       # db, too
-      expect(Item.find(json['data']['id']).description).to eql 'brand new item'
+      expect(Item.find(json["data"]["id"]).description).to eql "brand new item"
     end
   end
 
   describe "Item updating" do
-    
+
     before do
       @taxes = Tax.all
     end
-    
-    it 'PUT /api/v1/items/:id' do
-      mod = {'data'=> {'attributes' => {'quantity'=>33, 'tax_ids'=>[@taxes[2].id]}}}
+
+    it "PUT /api/v1/items/:id" do
+      mod = {"data"=> {"attributes" => {"quantity"=>33, "tax_ids"=>[@taxes[1].id]}}}
       put api_v1_item_path(@item), mod.to_json, @headers
       expect(response).to have_http_status :ok
-      expect(json['data']['attributes']['quantity']).to eql '33.0' 
-      expect(json['data']['attributes']['tax-ids'][0]).to eql @taxes[2].id
+      expect(json["data"]["attributes"]["quantity"]).to eql "33.0"
+      expect(json["data"]["attributes"]["tax-ids"][0]).to eql @taxes[1].id
       item = Item.find @item.id
       # the db, too
-      expect(item.taxes[0].id).to eql @taxes[2].id
+      expect(item.taxes[0].id).to eql @taxes[1].id
     end
   end
 
   describe "Item deletion" do
-    
-    it 'DELETE /api/v1/items/:id' do
+
+    it "DELETE /api/v1/items/:id" do
       delete api_v1_item_path(@item), nil, @headers
       expect(response).to have_http_status :no_content
       expect(Item.find_by_id(@item.id)).to be_nil
     end
   end
-
-  
-
 end
--- spec/requests/api/v1/api_v1_taxes_spec.rb
+++ spec/requests/api/v1/api_v1_taxes_spec.rb
@@ -1,79 +1,81 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Api::V1::Taxes", type: :request do
-
+RSpec.describe "Api::V1::Taxes:", type: :request do
   before do
-    Rails.cache.clear
     FactoryGirl.create :token
     FactoryGirl.create :user
-    @headers = {'Content-Type' => 'application/json', 
-        'Authorization' => 'Token token="123token"'}
-    @tax = FactoryGirl.create :tax
-    @retention = FactoryGirl.create :tax_retention
+
+    @headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Token token=\"#{Settings.api_token}\""
+    }
+
+    @vat = FactoryGirl.create :vat
+    @retention = FactoryGirl.create :retention
   end
 
-  describe 'Taxes show' do
-    it 'GET /api/v1/taxes/:id' do
-      get api_v1_tax_path(@tax), nil, @headers
+  describe "Taxes show" do
+    it "GET /api/v1/taxes/:id" do
+      get api_v1_tax_path(@vat), nil, @headers
       expect(response).to be_success
-      expect(json['data']['attributes']['name']).to eql 'VAT 21%'
+      expect(json["data"]["attributes"]["name"]).to eql "VAT"
     end
   end
 
-  describe 'Taxes listing' do
-    it 'GET /api/v1/taxes' do
+  describe "Taxes listing" do
+    it "GET /api/v1/taxes" do
       get api_v1_taxes_path, nil, @headers
       expect(response).to be_success
-      expect(json['data'].length).to eql 2
-      expect(json['data'][1]['attributes']['name']).to eql 'RETENTION'
+      expect(json["data"].length).to eql 2
+      expect(json["data"][1]["attributes"]["name"]).to eql "RETENTION"
     end
   end
 
-  describe 'Taxes creation' do
-    it 'POST /api/v1/taxes' do
+  describe "Taxes creation" do
+    it "POST /api/v1/taxes" do
       tx = {
-        'data' => {
-          'type' => 'taxes',
-          'attributes' => {
-            'name' => 'newTAX',
-            'value' => 33
-          } 
+        "data" => {
+          "type" => "taxes",
+          "attributes" => {
+            "name" => "newTAX",
+            "value" => 33
+          }
         }
       }
       post api_v1_taxes_path, tx.to_json, @headers
       expect(response).to be_success
-      expect(json['data']['attributes']['name']).to eql 'newTAX'
+      expect(json["data"]["attributes"]["name"]).to eql "newTAX"
       # in db, too
-      expect(Tax.find(json['data']['id']).name).to eql 'newTAX'
+      expect(Tax.find(json["data"]["id"]).name).to eql "newTAX"
       # location header
-      expect(response.headers['Location']).to eql api_v1_tax_url(json['data']['id'])
+      expect(response.headers["Location"]).to eql api_v1_tax_url(json["data"]["id"])
     end
   end
 
-  describe 'Tax updating' do
-    it 'PUT /api/v1/taxes/:id' do
+  describe "Tax updating" do
+    it "PUT /api/v1/taxes/:id" do
       mod = {
-        'data' => {
-          'attributes' => {
-            'name' => 'modTAX',
-            'default' => true
-          } 
+        "data" => {
+          "attributes" => {
+            "name" => "modTAX",
+            "default" => true
+          }
         }
       }
-      put api_v1_tax_url(@tax), mod.to_json, @headers
+      put api_v1_tax_url(@vat), mod.to_json, @headers
       expect(response).to be_success
       # name modified
-      expect(json['data']['attributes']['name']).to eql 'modTAX'
+      expect(json["data"]["attributes"]["name"]).to eql "modTAX"
       # in db, too
-      expect(Tax.find(json['data']['id']).name).to eql 'modTAX'
+      expect(Tax.find(json["data"]["id"]).name).to eql "modTAX"
     end
   end
 
-  describe 'Tax deletion' do
-    it 'DELETE /api/v1/taxes/:id' do
-      delete api_v1_tax_path(@tax), nil, @headers
+  describe "Tax deletion" do
+    it "DELETE /api/v1/taxes/:id" do
+      delete api_v1_tax_path(@vat), nil, @headers
       expect(response).to have_http_status :no_content
-      expect(Tax.find_by_id(@tax.id)).to be_nil
+      expect(Tax.find_by_id(@vat.id)).to be_nil
     end
   end
 
