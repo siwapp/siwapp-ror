994d1f8013819fd8e2c11df71702cf248becb940
Carlos Escribano <carlosescri@gmail.com>
2016-12-30 13:26:55 +0100

    #226 - Refactoring feature specs login
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/features/sessions/security_spec.rb
+++ spec/features/sessions/security_spec.rb
@@ -1,20 +1,17 @@
 require "rails_helper"
 
 feature "Sessions:" do
-
   scenario "Logged-in users can access protected routes" do
-    # TODO: Add more routes
+    # Any ApplicationController child controller (except SessionsController)
+    # is protected, so we can safely test only of them.
     visit invoices_path
     expect(page.current_path).to eql invoices_path
     visit root_path
     expect(page.current_path).to eql root_path
   end
 
-  scenario "Logged-out users can't access protected routes" do
-    first(:link, "Log out").click
-    expect(page.current_path).to eql login_path
+  scenario "Not-logged users can't access protected routes", :not_logged do
     visit invoices_path
     expect(page.current_path).to eql login_path # where are you going, sailor?
   end
-
 end
