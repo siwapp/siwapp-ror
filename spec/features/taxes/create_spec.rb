1b384ea1730f5ee234a7cd79eabe148ea3aad151
Carlos Escribano <carlosescri@gmail.com>
2016-12-30 16:10:32 +0100

    #226 - Taxes feature tests revisited
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/features/taxes/create_spec.rb
+++ spec/features/taxes/create_spec.rb
@@ -1,29 +1,28 @@
 require "rails_helper"
 
 feature "Taxes:" do
-
   scenario "User can create a tax", :js => true, :driver => :webkit do
-    visit "/taxes/new"
+    visit taxes_path
+    click_on "New Tax"
+
+    expect(page.current_path).to eql new_tax_path
 
     fill_in "Name", with: "IVA"
     fill_in "Value", with: "15"
-
     check "Enabled"
     check "Apply to new items by default"
 
     click_button "Save"
 
-    expect(page.current_path).to eql(taxes_path)
-    expect(page).to have_content("Tax was successfully created")
+    expect(page.current_path).to eql taxes_path
+    expect(page).to have_content "successfully created"
   end
 
   scenario "User can't create a tax with invalid data", :js => true, :driver => :webkit do
-    visit "/taxes/new"
+    visit new_tax_path
+    click_on "Save"
 
-    click_button "Save"
-
-    expect(page.current_path).to eql(taxes_path)
-    expect(page).to have_content("3 errors")
+    expect(page.current_path).to eql taxes_path
+    expect(page).to have_content "3 errors"
   end
-
 end
--- spec/features/taxes/list_spec.rb
+++ spec/features/taxes/list_spec.rb
@@ -1,23 +1,24 @@
 require "rails_helper"
 
 feature "Taxes:" do
-
   scenario "User can mark a tax as default from the list of taxes", :js => true, :driver => :webkit do
     FactoryGirl.create(:vat, default: true)
     FactoryGirl.create(:retention)
 
-    visit "/taxes"
+    click_on "Account"
+    click_on "Taxes"
 
-    expect(page).to have_content("VAT")
-    expect(page).to have_content("RETENTION")
-    expect(page).to have_checked_field('default_tax_1')   # VAT
-    expect(page).to have_unchecked_field('default_tax_2') # RETENTION
+    expect(page.current_path).to eql taxes_path
 
-    find_field('default_tax_2').click
+    expect(page).to have_content "VAT"
+    expect(page).to have_content "RETENTION"
+    expect(page).to have_checked_field "default_tax_1"   # VAT
+    expect(page).to have_unchecked_field "default_tax_2" # RETENTION
 
-    expect(page.current_path).to eql(taxes_path)
-    expect(page).to have_checked_field('default_tax_1') # VAT
-    expect(page).to have_checked_field('default_tax_2') # RETENTION
+    find_field("default_tax_2").click
+
+    expect(page.current_path).to eql taxes_path
+    expect(page).to have_checked_field "default_tax_1" # VAT
+    expect(page).to have_checked_field "default_tax_2" # RETENTION
   end
-
 end
