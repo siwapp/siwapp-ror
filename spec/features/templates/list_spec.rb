3444bddecbac2085c7aed61a1675cdb6ceff17e5
Carlos Escribano <carlosescri@gmail.com>
2016-12-30 16:40:04 +0100

    #226 - Templates feature tests revisited
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/features/templates/list_spec.rb
+++ spec/features/templates/list_spec.rb
@@ -7,36 +7,31 @@ feature "Templates:" do
   end
 
   scenario "User can see a couple of default templates if no change was made", :js => true, :driver => :webkit do
-    visit "/templates"
+    click_on "Account"
+    click_on "Templates"
 
-    expect(page).to have_content("Email Default")
-    expect(page).to have_checked_field("email_default_template_#{@email_template.id}")
-    expect(page).not_to have_checked_field("print_default_template_#{@email_template.id}")
+    expect(page.current_path).to eql templates_path
 
-    expect(page).to have_content("Print Default")
-    expect(page).to have_checked_field("print_default_template_#{@print_template.id}")
-    expect(page).not_to have_checked_field("email_default_template_#{@print_template.id}")
+    expect(page).to have_content "Email Default"
+    expect(page).to have_checked_field "email_default_template_#{@email_template.id}"
+    expect(page).not_to have_checked_field "print_default_template_#{@email_template.id}"
+
+    expect(page).to have_content "Print Default"
+    expect(page).to have_checked_field "print_default_template_#{@print_template.id}"
+    expect(page).not_to have_checked_field "email_default_template_#{@print_template.id}"
   end
 
   scenario "User can change the default template from the list of templates", :js => true, :driver => :webkit do
-    visit "/templates"
+    visit templates_path
 
     find_field("email_default_template_#{@print_template.id}").click
 
-    expect(page).to have_checked_field("print_default_template_#{@print_template.id}")
-    expect(page).to have_checked_field("email_default_template_#{@print_template.id}")
+    expect(page).to have_checked_field "print_default_template_#{@print_template.id}"
+    expect(page).to have_checked_field "email_default_template_#{@print_template.id}"
 
     find_field("print_default_template_#{@email_template.id}").click
 
-    expect(page).to have_checked_field("print_default_template_#{@email_template.id}")
-    expect(page).to have_checked_field("email_default_template_#{@print_template.id}")
-  end
-
-  scenario "User can access the edit template page from the list of templates", :js => true, :driver => :webkit do
-    visit "/templates"
-
-    click_link "Print Default"
-
-    expect(page.current_path).to eql edit_template_path(@print_template)
+    expect(page).to have_checked_field "print_default_template_#{@email_template.id}"
+    expect(page).to have_checked_field "email_default_template_#{@print_template.id}"
   end
 end
--- spec/features/templates/update_spec.rb
+++ spec/features/templates/update_spec.rb
@@ -1,30 +1,31 @@
 require "rails_helper"
 
 feature "Templates:" do
-  background do
-    @template = FactoryGirl.create(:print_template)
-  end
-
   scenario "User can update an existing template", :js => true, :driver => :webkit do
-    visit "/templates/1/edit"
+    template = FactoryGirl.create(:print_template)
+
+    visit templates_path
+    click_on "Print Default"
+
+    expect(page.current_path).to eql edit_template_path(template)
 
     fill_in "template_name", with: "My Awesome Template (modified)"
     fill_in "template_template", with: "Hello (modified)"
-
     click_on "Save"
 
     expect(page.current_path).to eql templates_path
-    expect(page).to have_content("My Awesome Template (modified)")
+    expect(page).to have_content "My Awesome Template (modified)"
   end
 
   scenario "User can't update an existing template with invalid data", :js => true, :driver => :webkit do
-    visit "/templates/1/edit"
+    template = FactoryGirl.create(:print_template)
+
+    visit edit_template_path(template)
 
     fill_in "template_name", with: ""
-
     click_on "Save"
 
-    expect(page.current_path).to eql template_path(@template)
-    expect(page).to have_content("1 error")
+    expect(page.current_path).to eql template_path(template)
+    expect(page).to have_content "1 error"
   end
 end
