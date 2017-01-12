ff9a6200421c1526f87811c17f4d6e86386e93f7
Carlos Escribano <carlosescri@gmail.com>
2017-01-12 14:32:44 +0100

    #226 - Print preview tests. Added missing button.
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/features/invoices/printing_spec.rb
+++ spec/features/invoices/printing_spec.rb
@@ -2,30 +2,33 @@ require "rails_helper"
 
 feature "Invoices:" do
   background do
-    # load templates
     Rails.application.load_seed
+
+    @invoice = FactoryGirl.create(:invoice, :paid)
   end
 
   scenario "User will see a preview of a paid invoice inside an iframe an can download a PDF", js: true, driver: :webkit do
-    invoice = FactoryGirl.create(:invoice, :paid)
-
     visit invoices_path
-    click_link invoice.to_s
+    click_link @invoice.to_s
 
+    expect(page.current_path).to eql invoice_path(@invoice)
     expect(page).to have_xpath("//iframe")
     expect(page).to have_link("Download PDF")
 
     click_on "Download PDF"
 
-    expect(page.response_headers["Content-Disposition"]).to eq "attachment; filename=\"#{invoice.to_s}.pdf\""
+    expect(page.response_headers["Content-Disposition"]).to eq "attachment; filename=\"#{@invoice.to_s}.pdf\""
     expect(page.response_headers["Content-Type"]).to eq "application/pdf"
   end
 
   scenario "Template url shows template", js: true, driver: :webkit do
-    invoice = FactoryGirl.create(:invoice, :paid)
-
-    visit print_invoice_path(invoice)
-
+    visit print_invoice_path(@invoice)
     expect(page).to have_content("Billed:")
+  end
+
+  scenario "User can access the edit page from the print preview page", :js => true, :driver => :webkit do
+    visit invoice_path(@invoice)
+    click_on "Edit"
+    expect(page.current_path).to eq edit_invoice_path(@invoice)
   end
 end
