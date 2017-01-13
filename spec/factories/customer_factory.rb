5a96326d39eed65ba9b61143231d2932e7eaec2f
Carlos Escribano <carlosescri@gmail.com>
2017-01-13 13:46:35 +0100

    Fixes #244
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/factories/customer_factory.rb
+++ spec/factories/customer_factory.rb
@@ -1,8 +1,24 @@
 FactoryGirl.define do
   factory :customer do
     name "Test Customer"
+    name_slug { "#{name.gsub(/^[^\w]+|[^\w]+$/i, '').gsub(/[^\w]+/i, '-').downcase}" }
     identification "12345"
     email "customer@example.com"
+
+    # WARNING: DON'T USE FOR TESTS!!!
+    factory :demo_customer do
+      name { Faker::Company.unique.name }
+      identification { Faker::Code.unique.asin }
+      contact_person { Faker::Name.unique.name }
+      email { "info@#{name_slug}.com" }
+      invoicing_address do
+        address = Faker::Address.unique.street_address
+        address << "\n#{Faker::Address.unique.secondary_address}"
+        address << "\n#{Faker::Address.unique.zip} #{Faker::Address.city}"
+        address << "\nUSA"
+        address
+      end
+    end
   end
 
   factory :ncustomer, class: Customer do
