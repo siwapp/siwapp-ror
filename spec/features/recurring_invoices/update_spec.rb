cb752166266fdc58dbc1176b37d5d192538b9ad7
ecoslado <kike@doofinder.com>
2017-02-02 17:59:33 +0100

    removes puts.

--- spec/features/recurring_invoices/update_spec.rb
+++ spec/features/recurring_invoices/update_spec.rb
@@ -25,7 +25,6 @@ feature "Recurring Invoices:" do
   scenario "User can add a new item to an existing recurring invoice", :js => true, :driver => :webkit do
     recurring_invoice = FactoryGirl.create(:recurring_invoice)
     visit edit_recurring_invoice_path(recurring_invoice)
-    puts session[:redirect_to]
     click_on "Add Line"
 
     within(:xpath, '//*[@id="js-items-table"]/div[2]') do
