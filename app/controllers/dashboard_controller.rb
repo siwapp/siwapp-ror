fdce3846f229b94ba06d8d49c59cba4e37dfb5e5
peillis <enrique@markhaus.com>
2017-02-06 17:35:49 +0100

    fixes #250

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @objects = Invoice.with_status(:past_due).paginate(page: params[:page], per_page: 20)
+    @objects = Invoice.with_status(:past_due).order(:issue_date).paginate(page: params[:page], per_page: 20)
     respond_to do |format|
-      format.html { render :index, layout: 'infinite-scrolling' }
+      format.html {render :index, layout: 'infinite-scrolling'}
     end
   end
 end
