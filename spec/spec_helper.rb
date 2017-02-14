20766ac345ffc748c8929c2fe864182ca8905ae2
Carlos Escribano <carlosescri@gmail.com>
2017-02-14 16:30:12 +0100

    #181 - New wait_for_ajax helper (jQuery only)
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require './spec/support/helpers' # to access helper methods for rspec
+# Helper methods for RSpec
+require './spec/support/helpers'
+require './spec/support/wait_for_ajax'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
