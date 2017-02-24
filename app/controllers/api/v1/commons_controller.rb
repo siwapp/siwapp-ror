17a7edd2ff86fc33445dfd49414d3f3d39167edc
Manuel Domínguez <manuel.dogo@gmail.com>
2017-02-24 10:09:57 +0100

    Make invoices customer not nullable

--- app/controllers/api/v1/commons_controller.rb
+++ app/controllers/api/v1/commons_controller.rb
@@ -46,6 +46,30 @@ class Api::V1::CommonsController < Api::V1::BaseController
   def create
     instance = model.new(api_type_params)
     set_instance instance
+
+    # if there is no customer associated then create a new one
+    if api_type_params[:customer_id] == '' or !api_type_params.has_key? :customer_id # for API
+      if api_type_params[:identification] and api_type_params[:identification] != ''
+        # First check: by VAT_ID
+        customer = Customer.find_by_identification api_type_params[:identification]
+      elsif api_type_params[:name]
+        # Second check: by name
+        customer = Customer.where('lower(name) = ?', api_type_params[:name].downcase).first
+      end
+
+      if !customer
+        customer = Customer.create(
+          :name => api_type_params[:name],
+          :identification => api_type_params[:identification],
+          :email => api_type_params[:email],
+          :contact_person => api_type_params[:contact_person],
+          :invoicing_address => api_type_params[:invoicing_address],
+          :shipping_address => api_type_params[:shipping_address]
+        )
+      end
+      get_instance.update(:customer_id => customer.id)
+    end
+
     if get_instance.save
       # Check if there is any meta_attribute
       if params[:data][:meta]
@@ -83,28 +107,7 @@ class Api::V1::CommonsController < Api::V1::BaseController
           end
         end
       end
-      # if there is no customer associated then create a new one
-      if api_type_params[:customer_id] == '' or !api_type_params.has_key? :customer_id # for API
-        if api_type_params[:identification] and api_type_params[:identification] != ''
-          # First check: by VAT_ID
-          customer = Customer.find_by_identification api_type_params[:identification]
-        elsif api_type_params[:name]
-          # Second check: by name
-          customer = Customer.where('lower(name) = ?', api_type_params[:name].downcase).first
-        end
-
-        if !customer
-          customer = Customer.create(
-            :name => api_type_params[:name],
-            :identification => api_type_params[:identification],
-            :email => api_type_params[:email],
-            :contact_person => api_type_params[:contact_person],
-            :invoicing_address => api_type_params[:invoicing_address],
-            :shipping_address => api_type_params[:shipping_address]
-          )
-        end
-        get_instance.update(:customer_id => customer.id)
-      end
+      get_instance.save
       render json: get_instance, status: :created
     else
       render json: get_instance.errors, status: :unprocessable_entity
