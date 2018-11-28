f4cfd028a0665c0e5596d855d1a3c77989e7d22f
peillis <enrique@doofinder.com>
2018-11-28 18:09:25 +0100

    upgrading rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
 
   get    'login'   => 'sessions#new',      as: :login
