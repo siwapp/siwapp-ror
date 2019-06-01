e7b98aed7e0e3e766e27ca5a15b50c94fc3c913c
peillis <enrique@doofinder.com>
2019-06-01 19:54:40 +0200

    fixing some tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,14 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'shoulda/matchers'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
