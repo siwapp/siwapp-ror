4a86c6113a2c106511aa084d1a4e945246377556
peillis <enrique@doofinder.com>
2019-11-04 12:31:20 +0100

    test error

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  self.smtp_settings = {
-    address:              Settings.host,
-    port:                 Settings.port.to_i,
-    domain:               Settings.domain,
-    user_name:            Settings.user,
-    password:             Settings.password,
-    authentication:       Settings.authentication,
-    enable_starttls_auto: Settings.enable_starttls_auto == '1' ? true : false
-  }
+  # self.smtp_settings = {
+  #   address:              Settings.host,
+  #   port:                 Settings.port.to_i,
+  #   domain:               Settings.domain,
+  #   user_name:            Settings.user,
+  #   password:             Settings.password,
+  #   authentication:       Settings.authentication,
+  #   enable_starttls_auto: Settings.enable_starttls_auto == '1' ? true : false
+  # }
 end
