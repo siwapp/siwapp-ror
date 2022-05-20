233940e481796c1ae44c5c6d0b23c90cbad6cb6b
Javier Bonet <jbonet@doofinder.com>
2022-05-20 10:40:26 +0200

    Modify procfile

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RACK_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
